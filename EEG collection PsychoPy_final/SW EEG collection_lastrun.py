b9b714fe4192a7316ee9ae175247f20d490f2627
JX-Liu1 <136583631+JX-Liu1@users.noreply.github.com>
2025-03-09 18:48:50 +0800

    Update SW EEG collection_lastrun.py

--- EEG collection PsychoPy_final/SW EEG collection_lastrun.py
+++ EEG collection PsychoPy_final/SW EEG collection_lastrun.py
@@ -1,15 +1,6 @@
 ﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on Sun Mar  9 18:04:41 2025
-If you publish work using this script the most relevant publication is:
 
-    Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
-        PsychoPy2: Experiments in behavior made easy Behav Res 51: 195. 
-        https://doi.org/10.3758/s13428-018-01193-y
-
-"""
 
 # --- Import packages ---
 from psychopy import locale_setup
